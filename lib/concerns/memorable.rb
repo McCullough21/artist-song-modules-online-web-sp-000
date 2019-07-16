42e0eaecd2d212a9c5e696b4e80d6012248a87a3
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-16 16:25:34 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,17 +1,17 @@
 module Memorable
   module ClassMethods
     def reset_all
-      all.clear
+      self.all.clear
     end
 
     def count
-      all.count
+      self.all.count
     end
   end
 
   module InstanceMethods
     def initialize
-      all << self
+      self.class.all << self
     end
   end
 end
