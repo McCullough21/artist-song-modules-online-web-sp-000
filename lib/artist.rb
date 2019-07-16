5f1f493010cc638a0abed022e367acb11d26cbc8
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-16 16:22:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,11 +11,10 @@ class Artist
 
   @@artists = []
 
-  # def initialize
-  #   @@artists << self
-  #   @songs = []
-  # end
-
+  def initialize
+      super
+      @songs = []
+    end
   # def self.find_by_name(name)
   #   @@artists.detect{|a| a.name == name}
   # end
